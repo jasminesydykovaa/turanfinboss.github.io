c1bf0b0050752b42844595558e105d9fbf474900
jasminesydykovaa <159644212+jasminesydykovaa@users.noreply.github.com>
2024-02-10 23:13:49 +0600

    Add files via upload

---
+++ finboss/html/script.js
@@ -0,0 +1,52 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+var courseSwiper = new Swiper(".course-slider", {
+    container: ".course-slider", // Corrected here
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var teachersSwiper = new Swiper(".teachers-slider", {
+    container: ".teachers-slider", // Corrected here
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
+
